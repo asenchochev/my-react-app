2b76752a1ea6a9b221f14d21470b76ffba7b51e4
Asen Chochev <asen.qkiqt145@gmail.com>
2025-03-05 21:37:06 +0200

    added useNavigate() to button "Създай Профил"

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { assets } from "../assets/assets";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+
+    const navigate = useNavigate();
+
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img className="w-44 cursor-pointer" src={assets.logo} alt="Лого" />
@@ -18,7 +21,7 @@ const NavBar = () => {
         </NavLink>
       </ul>
       <div>
-      <button className="px-6 py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-semibold rounded-lg shadow-md hover:shadow-lg hover:scale-105 transition-all duration-300 uppercase tracking-wide">Създай профил</button>
+      <button onClick={() => navigate('/login')} className="px-6 py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-semibold rounded-lg shadow-md hover:shadow-lg hover:scale-105 transition-all duration-300 uppercase tracking-wide">Създай профил</button>
 
       </div>
     </div>
