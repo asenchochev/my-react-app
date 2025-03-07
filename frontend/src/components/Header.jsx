c9903cb8e5cc9f24729bdebe65410647f8efbaef
Asen Chochev <asen.qkiqt145@gmail.com>
2025-03-07 19:31:01 +0200

    update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const Header = () => {
   return (
     <div>
-      
+      {/* Left side */}
+      <div>
+        <p>
+        Записване на Часове <br /> С Доверени Доктори
+        </p>
+        <div>
+          <img src={assets.group_profiles} alt="" />
+          <p>Просто прегледайте нашия обширен списък с доверени лекари, 
+          насрочете срещата си безпроблемно</p>
+        </div>
+        <a href="">Резервирайте час <img src={assets.arrow_icon} alt="" /></a>
+      </div>
+
+      {/* Right Side */}
+      <div>
+        <img src={assets.header_img} alt="" />
+      </div>
     </div>
   )
 }
