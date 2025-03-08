27f20f2d56816642617684500328803250242146
Asen Chochev <asen.qkiqt145@gmail.com>
2025-03-08 18:22:42 +0200

    create TopDoctors.jsx

---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TopDoctors = () => {
+  return (
+    <div className='text-center mb-10'>
+       <h1 className="text-3xl font-semibold text-gray-800 sm:text-4xl leading-snug">
+    Най-добрите лекари за записване
+  </h1>
+  <p className="mt-3 text-base text-gray-500 sm:text-lg max-w-xl mx-auto leading-relaxed">
+  Открийте най-добрите специалисти и запазете час бързо и лесно при ТОП лекарите, на които можете да се доверите.</p>
+    </div>
+  )
+}
+
+export default TopDoctors
