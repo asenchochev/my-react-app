4da864f095c2415384c8bd778f544f34442f6363
Asen Chochev <asen.qkiqt145@gmail.com>
2025-03-17 21:20:45 +0200

    Import Footer to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile';
 import MyAppointment from './pages/MyAppointment';
 import Appointment from './pages/Appointment';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
       <Route path='/myAppointment' element={<MyAppointment />} />
       <Route path='/appointment/:doctorId' element={<Appointment />} />
     </Routes>
+    <Footer/>
       
     </div>
   );
