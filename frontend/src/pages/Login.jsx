8a0a03aca117241fec96b014ab31d71c24f5113f
Asen Chochev <asen.qkiqt145@gmail.com>
2025-03-26 22:01:45 +0200

    Create Login and Register form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [state, setState] = useState('Sign Up');
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState(''); 
+    const [name, setName] = useState('');
   return (
-    <div>
-      <h1>HEHE</h1>
-    </div>
+    <form className='min-h-[80vh] flex flex-col justify-center items-center'>
+      <h1 className='text-3xl font-bold mb-5'>{state}</h1>
+      <input
+        type='text'
+        placeholder='Име'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className='border p-2 w-80 mb-5'
+      />
+      <input
+        type='email'
+        placeholder='Емайл'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className='border p-2 w-80 mb-5'
+      />
+      <input
+        type='password'
+        placeholder='Парола'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className='border p-2 w-80 mb-5'
+      />
+      <button
+        type='submit'
+        className='bg-blue-500 text-white p-2 w-80 mb-5'
+      >
+        {state}
+      </button>
+      <p>
+        {state === 'Sign Up' ? 'Вече имате профил?' : 'Нямате профил?'}
+        <span
+          className='text-blue-500 cursor-pointer'
+          onClick={() => setState(state === 'Sign Up' ? 'Log In' : 'Sign Up')}
+        >
+          {state === 'Sign Up' ? ' Log In' : ' Sign Up'}
+        </span>
+      </p>    
+      </form>
   )
 }
 
