e515256470ddf6808d7c7b81b72a5ceb3b252bb8
Asen Chochev <asen.qkiqt145@gmail.com>
2025-03-29 23:26:27 +0200

    update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,15 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Успешна връзка с базата данни!: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Грешка: ${error.message}`);
     process.exit(1);
   }
-}
+};
 
 export default connectDB;
