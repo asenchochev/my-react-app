898a4a97bc358e78ac4cd86c701f51a3d5a3b5d2
Asen Chochev <asen.qkiqt145@gmail.com>
2025-03-29 23:26:35 +0200

    update Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import connectDB from './config/mongodb';
+import connectDB from './config/mongodb.js';
 
 
 //app config
 const app = express();
 const PORT = process.env.PORT || 4000;
+connectDB();
 
 
 //middleware
