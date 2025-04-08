48bb35f1500f15a852f63ddd9fa5b16d502183e4
Asen Chochev <asen.qkiqt145@gmail.com>
2025-04-08 23:28:30 +0300

    Create Catalog,jsx and module css

---
+++ client/src/components/catalogPageComponent/Catalog.jsx
@@ -0,0 +1,16 @@
+import OurAgents from "./OurAgents";
+import HeaderCatalog from "./HeaderCatalog";
+import ListCatalog from "./ListCatalog";
+
+export default function Catalog() {
+    return (
+        <>
+                <HeaderCatalog/>
+
+                <ListCatalog/>
+
+                <OurAgents/>
+
+        </>
+    );
+}
