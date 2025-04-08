4b2297ec49ee09befdce657344edb5138230a58a
Asen Chochev <asen.qkiqt145@gmail.com>
2025-04-09 00:47:51 +0300

    Create HeaderCreate

---
+++ client/src/components/createEstatePageComponent/HeaderCreate.jsx
@@ -0,0 +1,17 @@
+import styles from './CreateEstate.module.css'
+
+export default function HeaderCreate() {
+    return (
+            <div className="row g-0 align-items-center flex-column-reverse flex-md-row">
+                <div className="col-md-6 p-5 mt-lg-5">
+                    <h1 className="display-5 animated fadeIn mb-4">Create Property</h1>
+                </div>
+                <div className="col-md-6 animated fadeIn">
+                    <img className="img-fluid" src="src/assets/img/header.jpg" alt="" />
+                </div>
+                <p className={styles.pHeader} >
+                </p>
+
+            </div>
+    )
+}
